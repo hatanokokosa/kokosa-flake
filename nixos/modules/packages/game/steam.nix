e21e8735203a2bdcd71fe93707ae913546943f4d
hatanokokosa <wuyumagician@gmail.com>
2026-01-21 17:24:15 +0800

    add ge-proton in steam

--- nixos/modules/packages/game/steam.nix
+++ nixos/modules/packages/game/steam.nix
@@ -3,11 +3,14 @@
   programs = {
     gamemode.enable = true;
     steam = {
-      enable = true;
       dedicatedServer.openFirewall = true;
       gamescopeSession.enable = true;
       remotePlay.openFirewall = true;
       protontricks.enable = true;
+      enable = true;
+      extraCompatPackages = [
+        pkgs.proton-ge-bin
+      ];
     };
   };
 
