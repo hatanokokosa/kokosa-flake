8eec5adecc1d6c6524d3adf7b9d8da4a87a1d10f
hatanokokosa <wuyumagician@gmail.com>
2026-05-09 14:58:36 +0800

    fix man

--- home/dotfiles/fish/functions/man.fish
+++ home/dotfiles/fish/functions/man.fish
@@ -2,7 +2,7 @@ set -gx BAT_THEME "Catppuccin Latte"
 set -gx BAT_STYLE plain
 
 function man --wraps man --description 'Display man pages with bat'
-    set -lx MANPAGER "sh -c 'col -bx | bat -l man -p --paging=never | less -R -Ps'"
+    set -lx MANPAGER "sh -c 'col -bx | bat -l man -p --paging=always'"
     set -lx MANROFFOPT "-c"
 
     set -lx MANPATH (string join : $MANPATH)
@@ -22,5 +22,5 @@ function man --wraps man --description 'Display man pages with bat'
         set MANPATH "$fish_manpath":$MANPATH
     end
 
-    command man $argv
+    command man --prompt="" $argv
 end
