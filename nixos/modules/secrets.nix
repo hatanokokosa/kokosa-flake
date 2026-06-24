9e76e984d9a374b8aef08fe4f4fd9e482afb7390
hatanokokosa <wuyumagician@gmail.com>
2026-06-25 02:09:04 +0800

    throne

--- nixos/modules/secrets.nix
+++ nixos/modules/secrets.nix
@@ -7,8 +7,4 @@
     file = inputs.self + "/secrets/syncthing-gui-password.age";
     owner = "hatano";
   };
-
-  age.secrets.mihomo-subscription = {
-    file = inputs.self + "/secrets/mihomo-subscription.age";
-  };
 }
