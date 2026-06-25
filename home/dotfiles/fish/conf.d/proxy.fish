fd25a1cb6080eed05b77672212a275b333880b39
hatanokokosa <wuyumagician@gmail.com>
2026-06-25 13:26:03 +0800

    fixsth

--- home/dotfiles/fish/conf.d/proxy.fish
+++ home/dotfiles/fish/conf.d/proxy.fish
@@ -7,11 +7,12 @@ function proxy-sync
         set -l http_url "http://$__throne_proxy_host:$__throne_proxy_port"
         set -l socks_url "socks5h://$__throne_proxy_host:$__throne_proxy_port"
 
-        set -gx http_proxy $http_url https_proxy $http_url all_proxy $socks_url no_proxy $__throne_no_proxy
-        set -gx HTTP_PROXY $http_url HTTPS_PROXY $http_url ALL_PROXY $socks_url NO_PROXY $__throne_no_proxy
+        set -gx http_proxy $http_url
+        set -gx https_proxy $http_url
+        set -gx all_proxy $socks_url
+        set -gx no_proxy $__throne_no_proxy
     else
         set -e http_proxy https_proxy all_proxy no_proxy
-        set -e HTTP_PROXY HTTPS_PROXY ALL_PROXY NO_PROXY
     end
 end
 
