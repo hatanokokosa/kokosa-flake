75c4823d69d6a00336b968966175eebb756012fc
hatanokokosa <wuyumagician@gmail.com>
2026-07-26 22:25:17 +0800

    rm mihomo age

--- secrets.nix
+++ secrets.nix
@@ -1,9 +1,4 @@
 let
   master = "age1utasugq63v8spf2jlyjk3ma5l7c68f60qnug9n7swf459p4wrudqv48u48";
   kokosa = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINgl+EIk/QAxExeVE5hAkhTXLwPQtv2dXYpBZFoGVeSB";
-in {
-  "secrets/mihomo-subscription.age".publicKeys = [
-    master
-    kokosa
-  ];
-}
+in {}
