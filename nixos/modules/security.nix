ed08fd492d81c0a7e5162cf54640a32fb766d948
hatanokokosa <wuyumagician@gmail.com>
2026-08-11 03:01:39 +0800

    gpg

--- nixos/modules/security.nix
+++ nixos/modules/security.nix
@@ -1,4 +1,12 @@
-{...}: {
+{pkgs, ...}: {
+  programs.gnupg = {
+    agent = {
+      enable = true;
+      enableSSHSupport = true;
+      pinentryPackage = pkgs.pinentry-qt;
+    };
+  };
+
   security = {
     pam.services.systemd-run0 = {};
     sudo.enable = false;
