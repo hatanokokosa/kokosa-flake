2654d07617108ba96f80acf084c73d92f03ab79b
hatanokokosa <kokosaarisu@gmail.com>
2026-09-10 18:44:50 +0800

    2 splayer-next

--- nixos/modules/media/creator.nix
+++ nixos/modules/media/creator.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     kdePackages.kdenlive
+    splayer-next
     obs-studio
     v4l-utils
     openutau
     blender
     pureref
-    splayer
     haruna
     krita
   ];
